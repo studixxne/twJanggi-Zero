6e077df3692933693b4a0170b53e4f9f6bf67f7a
seongone <studioone@sogang.ac.kr>
2026-02-17 18:29:07 +0900

    env.py Draft Code complete

--- src/env.py
+++ src/env.py
@@ -11,20 +11,80 @@ class TwJanggiEnv:
         self.reset()
 
     def reset(self):
-        # TODO: 현재 상태를 초기 상태로 변환
         self.board = self._get_init_board()
         self.current_player = 1
-
+        self.turn = 0
+        self.king_enter = {1:False, -1:False}
         # 자, 상, 장 포로 보유 개수
         self.taken_piece = {1:[0, 0, 0], -1:[0, 0, 0]}
 
     def step(self, action):
-        # TODO: Action을 수행하고 (next_state, reward, done, info) 반환
-        pass
+        self.turn += 1
+        move = action_to_move(action)
+        done = False
+
+        # 기물 이동
+        if move[0] != -1:
+            row, col, next_row, next_col = move
+            piece = self.board[row][col]
+            next_loc_piece = self.board[next_row][next_col]
+            
+            # 기물 잡기
+            if next_loc_piece != Piece.EMPTY:
+                captured = 1 if abs(next_loc_piece) == Piece.HU else abs(next_loc_piece)
+                
+                # 왕이 잡힌 경우 체크
+                if captured == Piece.KING:
+                    done = True
+
+                self.taken_piece[self.current_player][captured-1] += 1
+
+            # 기물 최종 이동
+            self.board[next_row][next_col] = piece
+            self.board[row][col] = Piece.EMPTY
+
+            # 상대 진영에 들어갔을 때 승진 및 승리 조건 체크
+            if self._is_enemy_location(next_row):
+                if abs(piece) == Piece.JA:
+                    self.board[next_row][next_col] = piece * 4
+                elif abs(piece) == Piece.KING:
+                    self.king_enter[self.current_player] = True
+
+        # 포로 기물 배치
+        else:
+            _, piece, next_row, next_col = move
+            self.board[next_row][next_col] = piece * self.current_player
+            self.taken_piece[self.current_player][piece-1] -= 1
+
+        reward = 0
+        # 왕을 잡는 것에 성공한 경우 승리
+        if done:
+            reward = 1
+        elif not done:
+            # 상대가 왕의 기지에서 1턴 버틴 경우 패배
+            if self.king_enter[self.current_player * -1]:
+                done = True
+                reward = -1
+            # 계속된 수 반복으로 인한 무승부 처리
+            elif self.turn >= 150:
+                done = True
+                reward = 0
+
+        self.current_player *= -1
+        next_state = self._get_next_state()
+        info = {}
+
+        return next_state, reward, done, info
 
     def get_valid_actions(self):
-        # TODO: 현재 상태에서 가능한 Action을 반환
         valid_actions = np.zeros(self.action_size)
+        valid_moves = self._get_valid_moves()
+
+        for move in valid_moves:
+            action = move_to_action(move)
+            valid_actions[action] = 1
+
+        return valid_actions
 
     def _get_valid_moves(self):
         # 이동할 수 있는 경우의 수
@@ -55,7 +115,6 @@ class TwJanggiEnv:
         return False
        
     def _get_init_board(self):
-        # TODO: 새로운 Board 생성 후 반환
         board = np.array(
             [[-Piece.JANG, -Piece.KING, -Piece.SANG],
              [Piece.EMPTY, -Piece.JA, Piece.EMPTY],
@@ -64,18 +123,6 @@ class TwJanggiEnv:
 
         return board
 
-    def _get_canonical_board():
-        # TODO: 반전된 표준 보드 반환
-        pass
-
-    def _get_state(self):
-        # TODO: 현재 상태를 반환
-        pass
-
-    def _get_reward(self):
-        # TODO: 현재 상태에서 보상을 계산 및 반환
-        pass
-
-    def render(self):
-        # TODO: 현재 상태를 출력
+    def _get_next_state(self):
+        # TODO: 다음 상태에 대한 반전된 정보를 전달 (Training)
         pass
