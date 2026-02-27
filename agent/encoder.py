5f1c1e2b360246074176a744135f6eaaf25b9d66
seongone <studioone@sogang.ac.kr>
2026-02-27 14:41:15 +0900

    Remove state_queue in Encoder Class & get_tensor logic fix

--- agent/encoder.py
+++ agent/encoder.py
@@ -5,24 +5,17 @@ from collections import deque
 
 class TwJanggiEncoder:
     def __init__(self, T):
-        self.state_queue = deque(maxlen=T)
         self.M = 21
         self.L = 2
         self.T = T
 
-    # (4, 3, in_ch)의 Tensor로 변환
-    def get_tensor(self, state):
-        board, taken_piece, king_enter, turn, repeat, player = state
-        self.state_queue.append(state)
-        states = []
+    # (1, 4, 3, in_ch)의 Tensor로 변환
+    def get_tensor(self, state_list):
+        _, _, _, turn, _, player = state_list[-1]
+        layers = []
 
-        # M*T개 쌓기
-        for _ in range(self.T-len(self.state_queue)):
-            states.append(np.zeros((self.M, 4, 3)))
-
-        for s in self.state_queue:
-            s_board, s_taken_piece, s_king_enter, s_turn, s_repeat, _ = self._get_flip_state(s, player)
-            layers = []
+        for state in state_list[::-1]:
+            s_board, s_taken_piece, s_king_enter, s_turn, s_repeat, _ = self._get_flip_state(state, player)
 
             # 내 기물의 위치 / 상대 기물의 위치 (10개)            
             target_piece = np.array([1, 2, 3, 4, 5, -1, -2, -3, -4, -5]).reshape(10, 1, 1)
@@ -39,16 +32,15 @@ class TwJanggiEncoder:
             king_enter_array = np.array((s_king_enter[1], s_king_enter[-1]), dtype=np.float32).reshape(2, 1, 1)
             layers.append(np.broadcast_to(king_enter_array, (2, 4, 3)))
 
-            result = np.concatenate(layers)
-            states.append(result)
+        layers.append(np.zeros((self.M*(self.T-len(state_list)), 4, 3)))
 
         # 누구의 턴 (레드(선공) 플레이어면 0, 초록(후공) 플레이어면 1)
-        states.append(np.zeros((1, 4, 3)) if player == 1 else np.ones((1, 4, 3)))
+        layers.append(np.zeros((1, 4, 3)) if player == 1 else np.ones((1, 4, 3)))
         # 총 이동 수
-        states.append(np.full((1, 4, 3), turn))
+        layers.append(np.full((1, 4, 3), turn))
         
         # (1, M*T+L, 4, 3)의 텐서 반환
-        x = torch.tensor(np.concatenate(states, axis=0), dtype=torch.float32).unsqueeze(0)
+        x = torch.tensor(np.concatenate(layers, axis=0), dtype=torch.float32).unsqueeze(0)
         return x
 
     def _get_flip_state(self, state, player):
