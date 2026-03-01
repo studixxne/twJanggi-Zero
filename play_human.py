04ec693b183a7b643c0d75d44f8c4aa9e5381d5b
seongone <studioone@sogang.ac.kr>
2026-03-02 00:15:32 +0900

    Human vs AI game impl

---
+++ play_human.py
@@ -0,0 +1,28 @@
+from agent.encoder import TwJanggiEncoder
+from agent.network import TwJanggiNet
+from agent.inference import Agent
+
+from src.game import TwJanggiGame
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="TwJanggi Human vs AlphaZero Play")
+
+    parser.add_argument('--model', type=str, default='data/master_model.pth', help='Model path ex) data/model.pth')
+    parser.add_argument('--T', type=int, default=4, help='Model Time step')
+    parser.add_argument('--hidden_ch', type=int, default=128, help='Model Hidden ch')
+    parser.add_argument('--num_block', type=int, default=4, help='Model Residual block num')
+
+    parser.add_argument('--first', type=int, default=True, help='Is human first player (1/0)')
+    parser.add_argument('--num_traversal', type=int, default=3000, help='MCTS Traversal num (Difficulty level)')
+
+    args = parser.parse_args()
+
+    encoder = TwJanggiEncoder(args.T)
+    network = TwJanggiNet(args.T*21+2, args.hidden_ch, args.num_block)
+    agent = Agent(encoder, network, num_traversal=args.num_traversal, model_path=args.model)
+    
+    game = TwJanggiGame(agent=agent, mode='battle', agent_turn=-1 if args.first==1 else 1)
+    game.run()
+    # 내가 선이면
