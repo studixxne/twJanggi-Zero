4abd5821bf2f004ac5145fd1fb2c1644f1e59f1d
seongone <studioone@sogang.ac.kr>
2026-03-01 20:59:28 +0900

    Add argparse to arena.py

--- arena.py
+++ arena.py
@@ -1,3 +1,7 @@
+from agent.encoder import TwJanggiEncoder
+from agent.network import TwJanggiNet
+from agent.inference import Agent
+from src.game import TwJanggiGame
 from src.utils import action_to_text
 
 class Arena:
@@ -65,4 +69,39 @@ class Arena:
         print(f"Agent 2 Win Rate: {(agent2_win) / (play_games*2 - draws + 1e-7) * 100:.1f}%")
         print("============================")
         return agent1_win, agent2_win, draws
+    
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="TwJanggi Arena (Model Performance Test)")
+
+    parser.add_argument('--model1', type=str, required=True, help='Agent1 Model path ex) data/model1.pth')
+    parser.add_argument('--model2', type=str, required=True, help='Agent2 Model path ex) data/model2.pth')
+
+    parser.add_argument('--num_games', type=int, default=10, help='total games')
+    parser.add_argument('--num_traversal', type=int, default=1000, help='MCTS traversal num')
+    
+    parser.add_argument('--T1', type=int, default=4, help='Model1 Time step')
+    parser.add_argument('--hidden_ch1', type=int, default=128, help='Model1 Hidden ch')
+    parser.add_argument('--num_block1', type=int, default=4, help='Model1 Residual block num')
+
+    parser.add_argument('--T2', type=int, default=4, help='Model2 Time step')
+    parser.add_argument('--hidden_ch2', type=int, default=128, help='Model2 Hidden ch')
+    parser.add_argument('--num_block2', type=int, default=4, help='Model2 Residual block num')
+
+    args = parser.parse_args()
+
+    encoder1 = TwJanggiEncoder(args.T1)
+    network1 = TwJanggiNet(args.T1*21+2, args.hidden_ch1, args.num_block1)
+
+    encoder2 = TwJanggiEncoder(args.T2)
+    network2 = TwJanggiNet(args.T2*21+2, args.hidden_ch2, args.num_block2)
+
+    agent1 = Agent(encoder1, network1, num_traversal=args.num_traversal, model_path=args.model1)
+    agent2 = Agent(encoder2, network2, num_traversal=args.num_traversal, model_path=args.model2)
+    
+    game = TwJanggiGame()
+    arena = Arena(agent1, agent2, game)
+
+    arena.run(args.num_games)
 
