62b29a999c75f44c4a9746141777985d16ad360d
seongone <studioone@sogang.ac.kr>
2026-03-06 11:26:23 +0900

    change default setting

--- analyze.py
+++ analyze.py
@@ -9,18 +9,19 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="TwJanggi Analyze")
 
-    parser.add_argument('--model', type=str, default='data/master_model.pth', help='Model path ex) data/model.pth')
+    parser.add_argument('--model', type=str, default='model/master_model.pth', help='Model path ex) model/model.pth')
     parser.add_argument('--T', type=int, default=4, help='Model Time step')
     parser.add_argument('--hidden_ch', type=int, default=128, help='Model Hidden ch')
     parser.add_argument('--num_block', type=int, default=4, help='Model Residual block num')
 
-    parser.add_argument('--num_traversal', type=int, default=1000, help='MCTS Traversal num')
+    parser.add_argument('--num_deep_traversal', type=int, default=5000, help='Hint MCTS Deep Traversal num')
+    parser.add_argument('--num_traversal', type=int, default=1000, help='Hint MCTS Traversal num')
 
     args = parser.parse_args()
 
     encoder = TwJanggiEncoder(args.T)
     network = TwJanggiNet(args.T*21+2, args.hidden_ch, args.num_block)
-    agent = Agent(encoder, network, num_traversal=args.num_traversal, model_path=args.model)
+    agent = Agent(encoder, network, model_path=args.model)
     
-    game = TwJanggiGame(agent=agent, mode='analyze')
+    game = TwJanggiGame(agent=agent, mode='analyze', num_traversal=args.num_traversal, num_deep_traversal=args.num_deep_traversal)
     game.run()
--- play_human.py
+++ play_human.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="TwJanggi Human vs AlphaZero Play")
 
-    parser.add_argument('--model', type=str, default='data/master_model.pth', help='Model path ex) data/model.pth')
+    parser.add_argument('--model', type=str, default='model/master_model.pth', help='Model path ex) model/model.pth')
     parser.add_argument('--T', type=int, default=4, help='Model Time step')
     parser.add_argument('--hidden_ch', type=int, default=128, help='Model Hidden ch')
     parser.add_argument('--num_block', type=int, default=4, help='Model Residual block num')
 
     parser.add_argument('--first', type=int, default=True, help='Is human first player (1/0)')
-    parser.add_argument('--num_traversal', type=int, default=3000, help='MCTS Traversal num (Difficulty level)')
+    parser.add_argument('--num_traversal', type=int, default=5000, help='MCTS Traversal num')
 
     args = parser.parse_args()
 
     encoder = TwJanggiEncoder(args.T)
     network = TwJanggiNet(args.T*21+2, args.hidden_ch, args.num_block)
-    agent = Agent(encoder, network, num_traversal=args.num_traversal, model_path=args.model)
+    agent = Agent(encoder, network, model_path=args.model)
     
-    game = TwJanggiGame(agent=agent, mode='battle', agent_turn=-1 if args.first==1 else 1)
+    game = TwJanggiGame(agent=agent, mode='battle', num_traversal=args.num_traversal, agent_turn=-1 if args.first==1 else 1)
     game.run()
